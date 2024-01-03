9646e4e4b9d9a5a64b23b9c142f4fe12f1423aa6
Vinicius <65102990+ViniciusLopesN@users.noreply.github.com>
2024-01-03 14:10:02 -0300

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
   return Number(a)/Number(b);
 }
+
+let firstNumber;
+let secondNumber;
+let operand; // used inside functio, check later.
+
+function operate(operand, a, b) {
+  switch (operand) {
+    case '+':
+      return add(a, b);
+
+    case '-':
+      return subtract(a, b);
+
+    case '*':
+      return multiply(a, b);
+
+    case '/':
+      return divide(a, b);
+    
+    default:
+      break;
+  }
+}
+
