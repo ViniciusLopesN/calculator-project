641f7553651994b021bbfc7c880aa9fec1c3e155
Vinicius <65102990+ViniciusLopesN@users.noreply.github.com>
2024-01-03 18:27:17 -0300

    Added basic functionalities

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ function divide(a, b) {
   return Number(a)/Number(b);
 }
 
-let firstNumber;
-let secondNumber;
-let operand; // used inside functio, check later.
+let inputedFirstNumber;
+let inputedSecondNumber;
+let inputedOperand;
 
 function operate(operand, a, b) {
-  
+
   switch (operand) {
     case '+':
       return add(a, b);
@@ -27,14 +27,88 @@ function operate(operand, a, b) {
     case '-':
       return subtract(a, b);
 
-    case '*':
+    case 'x':
       return multiply(a, b);
 
-    case '/':
+    case '÷':
       return divide(a, b);
     
     default:
       break;
   }
 }
+const display = document.getElementById('display');
+const getButtons = document.querySelectorAll('.digit-button');
+const numberButtons = Array.from(getButtons).sort((a,b) => a.textContent - b.textContent);
+const equalButton = document.getElementById('equal');
+const clearButton = document.getElementById('clear');
+const deleteButton = document.getElementById('delete');
+const upperDisplay = document.getElementById('upper-display');
 
+function clearContent() {
+  inputedFirstNumber = '';
+  inputedOperand = '';
+  inputedSecondNumber = '';
+  upperDisplay.textContent = '';
+  display.textContent = '';
+  operandButtons.forEach(button => button.classList.remove('active'));
+
+}
+
+clearButton.addEventListener('click', clearContent)
+
+deleteButton.addEventListener('click', () => {
+  display.textContent = display.textContent.slice(0,-1);
+  inputedFirstNumber = display.textContent;
+})
+
+
+
+for (const button of numberButtons) {
+  button.addEventListener('click', (event) => {
+    operandButtons.forEach(button => {
+      if(button.classList.contains('active')) {
+        display.textContent = '';
+        button.classList.remove('active');
+      }
+    });
+    populateDisplay(event);
+  });
+} 
+
+
+function populateDisplay(event) {
+  display.textContent += event.currentTarget.textContent;
+}
+
+
+
+const getOperandButtons = document.querySelectorAll('.operand-button')
+const operandButtons = Array.from(getOperandButtons)
+
+for (const button of operandButtons) {
+  button.addEventListener('click', () => {
+    if(display.textContent) {
+      operandButtons.forEach(button => button.classList.remove('active'));
+      button.classList.toggle('active');
+
+      // if(inputedFirstNumber) {
+      //   console.log('yes');
+      //   operandButtons.forEach(button => button.classList.remove('active'));
+      //   button.classList.toggle('active');
+      //   evaluate();
+      // }
+
+      inputedOperand = button.textContent;
+      inputedFirstNumber = display.textContent;
+    }
+  });
+}
+
+function evaluate() {
+  inputedSecondNumber = display.textContent;
+  upperDisplay.textContent = `${inputedFirstNumber} ${inputedOperand} ${inputedSecondNumber} = `
+  display.textContent = operate(inputedOperand, inputedFirstNumber, inputedSecondNumber);
+}
+
+equalButton.addEventListener('click', evaluate);
